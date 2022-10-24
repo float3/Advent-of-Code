cac44008b91de65fc152457d1277b636f0fbb82d
float3 <86748455+float3@users.noreply.github.com>
2022-10-24 22:36:17 +0200

    error handling

--- 2021/day/1/rust/part1/sonar_sweep_part_one/src/main.rs
+++ 2021/day/1/rust/part1/sonar_sweep_part_one/src/main.rs
@@ -1,20 +1,29 @@
 use std::fs;
 
 fn main() {
+    let output = count_increases("../../../inputs.txt");
+
+    match output {
+        Some(count) => println!("The number of increases is: {}", count),
+        None => println!("Error"),
+    }
+}
+
+fn count_increases(path: &str) -> Option<i32> {
     let mut increased_counter = 0;
 
-    let file_contents = fs::read_to_string("../../../inputs.txt").unwrap().replace("\r","");
+    let file_contents = fs::read_to_string(path).ok()?.replace("\r","");
 
     let mut splits = file_contents.split("\n");
-    let mut last_value = splits.next().unwrap().parse::<i32>().unwrap();
+    let mut last_value = splits.next()?.parse::<i32>().ok()?;
 
     for current_line in splits {
-        let current_value = str::parse::<i32>(current_line).unwrap();
+        let current_value = str::parse::<i32>(current_line).ok()?;
         if current_value > last_value {
             increased_counter += 1;
         }
         last_value = current_value;
     }
 
-    println!("{increased_counter}");
+    Some(increased_counter)
 }
