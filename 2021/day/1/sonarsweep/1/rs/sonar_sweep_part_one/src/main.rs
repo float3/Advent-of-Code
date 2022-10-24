912480a7cd5151016f9ffbe6533ef20acb591fab
float3 <86748455+float3@users.noreply.github.com>
2022-10-24 19:27:34 +0200

    Advent of Code 2021 Day 1 Sonar Sweep part one

---
+++ 2021/day/1/sonarsweep/1/rs/sonar_sweep_part_one/src/main.rs
@@ -0,0 +1,19 @@
+use std::fs;
+
+fn main() {
+    let mut increased_counter = 0;
+
+    let file_contents = fs::read_to_string("../../../inputs.txt").unwrap();
+    let mut splits = file_contents.split("\n");
+    let mut last_value = splits.next().unwrap().parse::<i32>().unwrap();
+
+    for current_line in splits {
+        let current_value = str::parse::<i32>(current_line).unwrap();
+        if current_value > last_value {
+            increased_counter += 1;
+        }
+        last_value = current_value;
+    }
+
+    println!("{increased_counter}");
+}
