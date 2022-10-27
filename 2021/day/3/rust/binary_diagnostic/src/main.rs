9b8e8d48873f083c1ce141d53c3f63b826d4e917
float3 <86748455+float3@users.noreply.github.com>
2022-10-27 17:13:40 +0200

    2021 day 3

--- 2021/day/3/rust/binary_diagnostic/src/main.rs
+++ 2021/day/3/rust/binary_diagnostic/src/main.rs
@@ -1,27 +1,68 @@
-use std::fs;
+#![feature(drain_filter)]
 
-const INPUT_FILE: &str = "../../inputs.txt";
+const WIDTH: usize = 12;
+const COUNT: usize = 1000;
 
-fn main() {
-    let power_consumption = power_consumption(INPUT_FILE);
+pub fn main() {
+    let gamma = calculate_gamma(include_str!("../../../inputs.txt"));
 
-    match power_consumption {
-        Some(count) => println!("The distance traveled is: {}", count),
+    match gamma {
+        Some(gamma) => println!("{}", gamma * (!gamma & ((1 << WIDTH) - 1))),
         None => println!("Error"),
     }
 
+    let oxygen = calculate_oxygen(include_str!("../../../inputs.txt"));
+
+    match oxygen {
+        Some(oxygen) => println!("{}", oxygen),
+        None => println!("Error"),
+    }
 }
 
-fn power_consumption(path: &str) -> Option<i32> {
-    let file_contents = fs::read_to_string(path).ok()?.replace("\r", "");
-    let splits = file_contents.split("\n");
+fn calculate_gamma(input: &str) -> Option<u32> {
+    let gamma = input
+        .lines()
+        .map(|l| usize::from_str_radix(l, 2).unwrap())
+        .fold(vec![0; WIDTH], |count, bits| {
+            count
+                .into_iter()
+                .enumerate()
+                .map(|(i, n)| n + ((bits & 1 << i) >> i))
+                .collect()
+        })
+        .into_iter()
+        .enumerate()
+        .map(|(i, b)| ((b >= COUNT / 2) as u32) << i)
+        .sum::<u32>();
 
-    let gamma = 0;
-    let epsilon = 0;
+    Some(gamma)
+}
 
-    for split in splits {
+fn calculate_oxygen(input: &str) -> Option<u32> {
+    let nums = input
+        .lines()
+        .map(|l| u32::from_str_radix(l, 2).unwrap())
+        .collect::<Vec<_>>();
 
-    }
+    let oxy = (0..WIDTH)
+        .rev()
+        .scan(nums.clone(), |oxy, i| {
+            let one = oxy.iter().filter(|n| *n & 1 << i > 0).count() >= (oxy.len() + 1) / 2;
+            oxy.drain_filter(|n| (*n & 1 << i > 0) != one);
+            oxy.first().copied()
+        })
+        .last()
+        .unwrap();
 
-    Some(2)
+    let co2 = (0..WIDTH)
+        .rev()
+        .scan(nums, |co2, i| {
+            let one = co2.iter().filter(|n| *n & 1 << i > 0).count() >= (co2.len() + 1) / 2;
+            co2.drain_filter(|n| (*n & 1 << i > 0) == one);
+            co2.first().copied()
+        })
+        .last()
+        .unwrap();
+
+    Some(oxy * co2)
 }
